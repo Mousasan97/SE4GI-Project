57654076f717861d11c605cf8eca35da3a22f534
unknown <juanfrancisco.amieva@mail.polimi.it>
2021-05-31 10:58:31 +0200

    Include K-means clustering output in Scatterplot

--- JAM/make_graphs.py
+++ JAM/make_graphs.py
@@ -6,6 +6,8 @@ import geopandas as gpd
 import requests
 import plotly.express as px
 import plotly.graph_objects as go
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.cluster import KMeans
 
 def dash_():
     
@@ -16,6 +18,18 @@ def dash_():
     data_df['lat'] = pd.to_numeric(data_df['4_Specify_the_positi.longitude'], errors='coerce')
     data_df['lon'] =  pd.to_numeric(data_df['4_Specify_the_positi.latitude'], errors='coerce')
     data_geodf = gpd.GeoDataFrame(data_df, geometry=gpd.points_from_xy(data_df['lon'], data_df['lat']))
+
+    #Encoders to transform categorical data into numbers
+    encoder1 = OrdinalEncoder(categories=[["0 - 0.5 m", "0.5 - 1 m", "1 - 2 m", "Higher than 2 m"]])
+    encoder2 = OrdinalEncoder(categories=[["Not sure","Low", "Middle","High"]])
+    
+    encoder1.fit(data_df[["8_Set_the_size_of_th"]])
+    data_df["Order_size"] = encoder1.transform(data_df[["8_Set_the_size_of_th"]])
+    
+    encoder2.fit(data_df[["9_Determine_the_leve"]])
+    data_df["Order_risk"] = encoder2.transform(data_df[["9_Determine_the_leve"]])
+    
+    df_clustering=data_df.loc[:,["Order_risk","Order_size"]]
 
     Size_distres=data_df.groupby(['8_Set_the_size_of_th']).count()
     Size_distres=Size_distres.iloc[:,1]
@@ -151,7 +165,7 @@ def dash_():
     fig6 = go.Figure(data=[go.Bar(x=i, y=j,
                 hovertext=i)])
     # Customize aspect
-    fig6.update_traces(marker_color='rgb(100,100,0)', marker_line_color='rgb(128,128,128)',
+    fig6.update_traces(marker_color='rgb(77,48,218)', marker_line_color='rgb(128,128,128)',
                       marker_line_width=1.5, opacity=0.6)
     fig6.update_layout(title_text='Requests of maintenance per user')
 
@@ -177,5 +191,42 @@ def dash_():
 
     fig7.write_html("templates/req_day_plot.html")    
     
+    kmeans = KMeans(n_clusters=3, random_state=0).fit(df_clustering)
+    
+    df_clustering["Cluster"]=kmeans.labels_
+    
+    df_clustering["Order_size"]=df_clustering["Order_size"].astype(int)
+    
+    fig8 = go.Figure()
+    
+    fig8.add_trace(
+            
+        go.Scatter(
+            x=df_clustering["Order_risk"],
+            y=df_clustering["Order_size"],
+            mode="markers",
+            marker=dict(size=20, color=df_clustering["Cluster"])
+            )
+    )
+    
+    fig8.update_layout(
+        autosize=True,
+         yaxis=dict(
+            title_text="Risk Level",
+            ticktext=["Not sure","Low", "Middle","High"],
+            tickvals=[0, 1, 2, 3]),
+         xaxis=dict(
+            title_text="Size of distress",
+            ticktext=["0 - 0.5 m", "0.5 - 1 m", "1 - 2 m", "Higher than 2 m"],
+            tickvals=[0, 1, 2, 3])
+         )     
+
+    fig8.update_layout(title_text='Clusters of maintenance requests')
+
+    fig8.update_layout(font_family="Roboto",font_color="white")
+
+    fig8.update_layout(template="plotly_dark")    
+
+    fig8.write_html("templates/scatter_clustering.html")
     
     
